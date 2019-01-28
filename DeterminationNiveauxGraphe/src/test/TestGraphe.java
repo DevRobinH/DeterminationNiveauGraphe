abd81f831d1190991cb2b785d83eb8b1bd75d340
q.massellamany <q.massellamany@314-3.formation-cci12.fr>
2019-01-28 10:01:53 +0100

    Test de la classe Graphe

--- DeterminationNiveauxGraphe/src/test/TestGraphe.java
+++ DeterminationNiveauxGraphe/src/test/TestGraphe.java
@@ -21,26 +21,38 @@ public class TestGraphe {
 										          {0,0,0,0,0,0,0,0},
 										          {0,0,0,0,0,0,0,0}};
 
+	public static final int MATRICE_TEST2[][] =  {{0,1,0,1,0,0,0},
+										          {0,0,1,0,0,0,1},
+										          {0,0,0,0,1,0,1},
+										          {0,0,0,0,1,1,0},
+										          {0,0,0,0,0,1,0},
+										          {0,0,0,0,0,0,0},
+										          {0,0,0,0,0,1,0}};
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		int matrice[][] = {{0,0,0,1,0,0,0,0},
-				           {0,0,0,1,1,0,0,0},
-				           {0,0,0,0,1,0,0,1},
-				           {0,0,0,0,0,1,1,1},
-				           {0,0,0,0,0,0,1,0},
-				           {0,0,0,0,0,0,0,0},
-				           {0,0,0,0,0,0,0,0},
-				           {0,0,0,0,0,0,0,0}};
 
-		int nbSommet = 8;
+		Graphe grapheTest = new Graphe(MATRICE_TEST1.length,MATRICE_TEST1);
 
-		Graphe grapheTest = new Graphe(nbSommet,matrice);
+        for(int i = 0; grapheTest.getListeSommet().size() < i; i++) {
 
-        System.out.println(grapheTest.getListeSommet().get(1).getSuccesseurs().get(1).getId());
+        	System.out.println("Sommet " + grapheTest.getListeSommet().get(i) + " :");
+        	System.out.println(" Liste de ses prédecesseurs : ");
+
+        	for (int j = 0; grapheTest.getListeSommet().get(i).getPredecesseurs().size() < j; j++) {
+        		System.out.print(grapheTest.getListeSommet().get(i).getPredecesseurs().get(j).getId() + ", ");
+        	}
+
+        	System.out.println(" Liste de ses sucesseurs : ");
+
+        	for (int j = 0; grapheTest.getListeSommet().get(i).getSuccesseurs().size() < j; j++) {
+        		System.out.print(grapheTest.getListeSommet().get(i).getSuccesseurs().get(j).getId() + ", ");
+        	}
+        }
 	}
 
 }
